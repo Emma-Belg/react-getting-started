00c4dad32f8748e2bc9149a19c9a841ec60e9a87
emma <emma.wishart@live.com.au>
2020-08-16 17:17:49 +0200

    push before reinstall

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './GithubCardsApp.css';
 
-/*const testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];*/
 
 const GitList = (props) => (
     <div>
         {/*this line is a mix of js & React. .map is a js function
         .map takes a function as an argument & converts the array befre the dot into a new array using the return values in the function.*/}
-        {props.profiles.map(profile => <Card {...profile} />)}
+        //because this is a dynamic element React wants me to use a unique key. As the profile.id is unique, we have used those as the key.
+        {props.profiles.map(profile => <Card key={profile.id}{...profile} />)}
     </div>
 );
 
--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './GithubCardsApp.css';
 import axios from 'axios';
 
+//Ways to improve this code
+    /*this is thought to be too much responsibility for one component, it both fetches and handles data
+    also your whole app should not depend on a library like axios, you should have a small agent type module
+    this 'agent' module's responsibility should be to communicate with APIs
+    you should then make your code only depend on this agent module*/
+    //You could also extract the logic about managing the state of the user input
 
 class Form extends React.Component {
     state = {userName: ''};
@@ -11,6 +17,7 @@ class Form extends React.Component {
         const resp = await
             axios.get(`https://api.github.com/users/${this.state.userName}`);
         this.props.onSubmit(resp.data)
+        //this is to reset the input box with an empty string, otherwise the last typed value would stay
         this.setState({userName:''})
 
         /* console.log(
